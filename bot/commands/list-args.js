87ada9c43c3752055fec063f7967f8f387d73d79
JosephJvB <jo-van-bo@hotmail.com>
2018-03-17 12:06:44 +1300

    edited response string

--- bot/commands/list-args.js
+++ bot/commands/list-args.js
@@ -1,5 +1,5 @@
 module.exports = (message, args) => {
   if(!args.size) return message.reply('Please give me some arguments to list!')
- return message.channel.send('Your args, m\lady: ', args)
+ return message.channel.send('Your args, m\'lady: ', args)
 }
 
