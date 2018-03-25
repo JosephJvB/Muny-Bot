f1fcd0a5875bad29c301818b152035d453caf0d0
JosephJvB <jo-van-bo@hotmail.com>
2018-03-25 16:10:00 +1300

    handle decimal input on bulk delete

--- src/commands/delete-bulk.js
+++ src/commands/delete-bulk.js
@@ -1,21 +1,27 @@
 module.exports = (message, args) => {
 
   const { channel } = message
-  const amount = args[0]
+  let amount = args[0]
   
   // handle errors
   if(!amount) {
     return channel.send('I need more direction in my life,\nplease tell me how many messages to delete')
   }
-  if(isNaN(Number(amount))) {
+  if(isNaN(amount)) {
     return channel.send(`Mayonnaise is not an instrument, Patrick.\nAnd '${amount}' is not a number`)
   }
 
-  if(amount < 1) return channel.send('I\'m only willing to delete messages if you give me a number greater than zero here.')
+  // remove decimal values
+  amount = Number(amount).toFixed(0)
 
-  // only delete 10 messages at a time
-  const capped_amount = amount > 10 ? 10 : amount
+  // max delete = 10
+  // min delete = 1
+  amount = amount > 10
+    ? 10
+    : amount < 1
+      ? 1
+      : amount
 
   // 2nd arg "true" prevents error if user tries to delete messages older than 2 weeks
-  channel.bulkDelete(capped_amount, true)
+  channel.bulkDelete(amount, true)
 }
